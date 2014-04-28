8e6b2ff5393e4f6c465c14b63543452697baddf9
Collective Archive (via Travis CI) <chris@geihsler.net>
2014-04-27 20:23:38 -0400

    Built collective-archive-site-generator from commit 19e9783 on branch master

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
+var triggerBuild = function(callback) {
+  $.get('http://collective-archive-deployer.herokuapp.com/deploy', {
+      success: function() {
+        console.log('success');
+        callback();
+      },
+      error: function() {
+        console.log('error');
+      }
+  });
+}
 
+$(function() {
+  $('.reload-content').click(function(e) {
+    $button = $(this);
+
+    triggerBuild(function() {
+      $('footer').html('<p>The site will be rebuilt shortly.</p>');
+    });
+
+    e.preventDefault();
+    return false;
+  });
+});
