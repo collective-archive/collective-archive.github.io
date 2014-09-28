c64a1f2a878aeeed20baea4212a4b53afeb31c9f
Collective Archive (via Travis CI) <chris@geihsler.net>
2014-09-28 13:16:13 +0000

    Built collective-archive-site-generator from commit caf0d78 on branch (unavailable)

--- scripts/main.js
+++ scripts/main.js
@@ -17,3 +17,21 @@ $(function() {
     return false;
   });
 });
+
+window.ImageCarousel = function() {
+  return {
+    init: function() {
+      $('.image-carousel').slick({
+        arrows: true,
+        dots: true,
+        prevArrow: '.image-carousel-prev',
+        nextArrow: '.image-carousel-next',
+        onInit: function(slider) {
+          if(slider.slideCount > 1) {
+            $('.image-carousel-button').show();
+          }
+        }
+      });
+    }
+  }
+}();
