df216d31535a1b617e5394868c04a889eeb71497
Chris Geihsler <chris@geihsler.net>
2015-02-28 16:59:31 -0500

    Built collective-archive-site-generator from commit b928081 on branch search

--- scripts/main.js
+++ scripts/main.js
@@ -45,26 +45,11 @@ window.CA = window.CA || {};
 CA.Search = function() {
   return {
     init: function() {
-      window.tipuesearch_stop_words = [
-        "and", "be", "by", "do", "for",
-        "he", "how", "if", "is", "it",
-        "my", "not", "of", "or", "the",
-        "to", "up", "what", "when"
-      ];
-
-      window.tipuesearch_replace = {
-        "words": [
-         {"word": "javscript", "replace_with": "javascript"}
-      ]};
-
-      window.tipuesearch_stem = {
-        "words": [
-         {"word": "e-mail", "stem": "email"},
-      ]};
-
-      $('#tipue_search_input').tipuesearch({
-        mode: 'json',
-        contentLocation: '/scripts/search_content.json'
+      $('#search-input').lunrSearch({
+        indexUrl: '/search_index.json',
+        results : '#search-results',
+        entries : '.entries',
+        template: '#search-results-template'
       });
     }
   };
