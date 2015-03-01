4fa05144742dec1fdd5c0f4600550ba5c0abf98e
Chris Geihsler <chris@geihsler.net>
2015-03-01 14:01:41 -0500

    Built collective-archive-site-generator from commit 62270c0 on branch master

--- scripts/main.js
+++ scripts/main.js
@@ -23,18 +23,9 @@ window.CA = window.CA || {};
 CA.ImageCarousel = function() {
   return {
     init: function() {
-      $('.image-carousel').slick({
-        arrows: true,
-        dots: true,
-        prevArrow: '.image-carousel-prev',
-        nextArrow: '.image-carousel-next',
-        onInit: function(slider) {
-          if(slider.slideCount > 1) {
-            $('.image-carousel-button').show();
-          }
-
-          $('.image-carousel img').show();
-        }
+      $('#image-carousel').carousel({
+        interval: false,
+        keyboard: true
       });
     }
   }
